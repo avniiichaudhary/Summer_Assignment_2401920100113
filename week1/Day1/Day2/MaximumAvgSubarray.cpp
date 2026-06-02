ca3701d626a7aa92a82603aa4dcb07bdda35160d
Avni Chaudhary <avniiichaudhary73@gmail.com>
2026-06-02 23:20:44 +0530

    Initial commit

---
+++ week1/Day1/Day2/MaximumAvgSubarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+
+        int sum=0;
+        for(int i=0;i<k;i++)
+        {
+            sum=sum+nums[i];
+        }
+        int maxi=sum;
+        for(int i=k;i<nums.size();i++)
+        {
+            sum=sum-nums[i-k]+nums[i];
+            maxi=max(sum,maxi);
+        }
+        return (double)maxi/k;
+    }
+};
