e84f09da70e473fa9f30dbba3639180505714800
avniiichaudhary <avniiichaudhary73@gmail.com>
2026-06-04 22:00:14 +0530

    Update Day 3 solutions

--- WEEK 1/Day3/MergeSorted.cpp
+++ WEEK 1/Day3/MergeSorted.cpp
@@ -4,7 +4,7 @@ public:
         int left = 0;
         int right = height.size() - 1;
         int area = 0;
-
+ 
         while (left < right) {
             int len = min(height[left], height[right]);
             area = max(area, (right - left) * len);
